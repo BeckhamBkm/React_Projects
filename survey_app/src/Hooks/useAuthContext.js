933515e268f3773817c90453ecb73c0978147006
Bokang Modise <Bokang.Modise@brolink.co.za>
2023-03-28 09:07:35 +0200

    Commit

---
+++ survey_app/src/Hooks/useAuthContext.js
@@ -0,0 +1,10 @@
+import { AuthContet } from "../context/AuthContext";
+import { useContext } from "react";
+
+export const useAuthContext = () =>{
+    const context = useContext(AuthContext)
+
+    if(!context){
+        throw Error('useAuthContext must be inside a AuthContextProvider')
+    }
+}
